5170a5dcfd895524b25d54673612b49d788aaef3
ChuckWaltz <chuckwaltz@gmail.com>
2020-02-01 07:38:21 -0500

    Updates to homepage top section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
     <div id="nav">
+      <div id="logo-container" class="wow fadeIn">
+        <img src="@/assets/dumbbell.svg" />
+      </div>
       <div id="nav-menu-button"></div>
+      <ul id="nav-list">
+        <li>ABOUT</li>
+        <li>SERVICES</li>
+        <li>INSTRUCTORS</li>
+        <li>CONTACT</li>
+      </ul>
       <!-- <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>-->
     </div>
@@ -10,13 +19,14 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Exo:700&display=swap");
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Exo", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,16 +35,37 @@
 
 #nav {
   position: fixed;
-  right: 0;
-
   background-color: #111111;
-  height: 100vh;
+  z-index: 100;
+  /* right: 0;
+  height: 100vh; */
+  height: 50px;
+  width: 100vw;
+  top: 0;
+
+  #logo-container {
+    position: relative;
+    float: left;
+    padding: 12px;
+    height: 100%;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
 
   #nav-menu-button {
-    width: 100%;
     background-color: #42b983;
-    width: 80px;
-    height: 70px;
+    width: 60px;
+    height: 100%;
+    float: right;
+  }
+
+  #nav-list {
+    display: none;
+    letter-spacing: 2px;
+    font-size: 18px;
   }
 
   a {
@@ -43,6 +74,63 @@
 
     &.router-link-exact-active {
       color: #42b983;
+    }
+  }
+}
+
+// Small devices (landscape phones, 576px and up)
+@media (min-width: 576px) {
+  #nav {
+    position: fixed;
+    right: 0;
+    height: 100vh;
+    width: 60px;
+
+    #logo-container {
+      position: fixed;
+      left: 0;
+      top: 0;
+      padding: 12px;
+      height: 60px;
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+
+    #nav-menu-button {
+      background-color: #42b983;
+      width: 60px;
+      height: 50px;
+      float: right;
+    }
+
+    #nav-list {
+      color: white;
+      position: absolute;
+      transform: translate(-100%);
+      display: flex;
+      height: 50px;
+      list-style: none;
+      align-items: center;
+      li {
+        margin-right: 25px;
+
+        &:hover {
+          cursor: pointer;
+          color: #42b983;
+        }
+      }
+    }
+
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,8 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <div id="top-section">
       <div id="ts-circle">
+        <div id="ts-image"></div>
         <div id="ts-circle-text">TRU FIT</div>
-      </div>
-      <div id="ts-image"></div>
-      <div id="logo-container">
-        <img src="@/assets/dumbbell.svg" />
       </div>
     </div>
   </div>
@@ -28,11 +25,15 @@ export default {
 
 <style lang="scss" scoped>
 .home {
+  height: 300vh;
 }
 
 #top-section {
   height: 100vh;
-  background: radial-gradient(circle at center, rgb(27, 27, 27), black 40%);
+  width: 100vw;
+  overflow: hidden;
+  position: relative;
+  background: radial-gradient(circle at center, rgb(58, 58, 58), black 40%);
 
   #ts-circle {
     position: absolute;
@@ -40,14 +41,16 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     border-radius: 50%;
-    border: 30px solid #42b983;
-    width: 55vh;
-    height: 55vh;
+    border: 5vh solid #42b983;
+    width: 70vh;
+    height: 70vh;
 
     #ts-circle-text {
+      text-align: center;
       color: white;
-      letter-spacing: 40px;
-      font-size: 20vw;
+      font-family: "Exo", sans-serif;
+      letter-spacing: 12px;
+      font-size: 20vmin;
       font-weight: bold;
       position: absolute;
       top: 50%;
@@ -59,26 +62,16 @@ export default {
 
   #ts-image {
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100vw;
+    height: 100vh;
     padding: 30px 40px;
     background-image: url("../assets/fitness-girl-cropped-update-2.png");
     background-size: auto 90%;
     background-repeat: no-repeat;
-    background-position: bottom;
-  }
-
-  #logo-container {
-    float: left;
-    width: calc(100px + 1.5vw);
-    margin-left: 40px;
-    margin-top: 30px;
-
-    img {
-      width: 100%;
-    }
+    background-position: center;
   }
 }
 </style>
