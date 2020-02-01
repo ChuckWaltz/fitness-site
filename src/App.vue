6e64c3d7e109a83a6f23cbc0563dddf15ebcabed
Chuck Waltz <chuckwaltz@gmail.com>
2020-01-31 20:09:03 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div id="nav-menu-button"></div>
+      <!-- <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>-->
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,7 +24,18 @@
 }
 
 #nav {
-  padding: 30px;
+  position: fixed;
+  right: 0;
+
+  background-color: #111111;
+  height: 100vh;
+
+  #nav-menu-button {
+    width: 100%;
+    background-color: #42b983;
+    width: 80px;
+    height: 70px;
+  }
 
   a {
     font-weight: bold;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div id="top-section">
+      <div id="ts-circle">
+        <div id="ts-circle-text">TRU FIT</div>
+      </div>
+      <div id="ts-image"></div>
+      <div id="logo-container">
+        <img src="@/assets/dumbbell.svg" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    //HelloWorld
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+}
+
+#top-section {
+  height: 100vh;
+  background: radial-gradient(circle at center, rgb(27, 27, 27), black 40%);
+
+  #ts-circle {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+    border: 30px solid #42b983;
+    width: 55vh;
+    height: 55vh;
+
+    #ts-circle-text {
+      color: white;
+      letter-spacing: 40px;
+      font-size: 20vw;
+      font-weight: bold;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100vw;
+    }
+  }
+
+  #ts-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 30px 40px;
+    background-image: url("../assets/fitness-girl-cropped-update-2.png");
+    background-size: auto 90%;
+    background-repeat: no-repeat;
+    background-position: bottom;
+  }
+
+  #logo-container {
+    float: left;
+    width: calc(100px + 1.5vw);
+    margin-left: 40px;
+    margin-top: 30px;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
